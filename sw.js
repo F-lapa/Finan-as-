3fdf84749534fda4696dcd38b6371f6913903627
F-lapa <fernandolapa1987@gmail.com>
2026-08-04 18:46:25 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = "financas-smart-v1";
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
